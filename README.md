1abbc9afd3deda17ee3e398dc6d82760c9de195a
hunghg255 <giahunghust@gmail.com>
2023-04-24 11:47:10 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Agile Css Autocomplete
 
+[Setup Demo](https://github.com/hunghg255/agile-css-atomic)
+
 ![Demo](https://res.cloudinary.com/hunghg255/image/upload/v1682195790/Screenshot_2023-04-23_at_03.34.43_rxcy1r.png)
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import { BREAK_POINT, CSS_PROPS, CSS_PSEUDO } from './constant';
 const regClass = /[className,class,\',\",\[]/;
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = {
+  const providerCssProps = {
     provideCompletionItems(
       document: vscode.TextDocument,
       position: vscode.Position,
@@ -35,6 +35,30 @@ export function activate(context: vscode.ExtensionContext) {
         return simpleCompletion;
       });
 
+      return arr1;
+    },
+  };
+
+  const providerCssPseudo = {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+      context: vscode.CompletionContext,
+    ) {
+      const line = document.lineAt(position.line);
+
+      if (!regClass.test(line?.text)) {
+        return [];
+      }
+
+      // const simpleCompletion1 = new vscode.CompletionItem({
+      // 	label: 'background-color',
+      // 	detail: " [bgc:]",
+      // 	description: "AgileCss"
+      // });
+      // simpleCompletion1.insertText = "bgc:";
+
       const arr2 = Object.keys(CSS_PSEUDO).map((key) => {
         const simpleCompletion = new vscode.CompletionItem({
           label: CSS_PSEUDO[key],
@@ -45,6 +69,30 @@ export function activate(context: vscode.ExtensionContext) {
 
         return simpleCompletion;
       });
+
+      return arr2;
+    },
+  };
+
+  const providerCssBreakpoint = {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+      context: vscode.CompletionContext,
+    ) {
+      const line = document.lineAt(position.line);
+
+      if (!regClass.test(line?.text)) {
+        return [];
+      }
+
+      // const simpleCompletion1 = new vscode.CompletionItem({
+      // 	label: 'background-color',
+      // 	detail: " [bgc:]",
+      // 	description: "AgileCss"
+      // });
+      // simpleCompletion1.insertText = "bgc:";
 
       const arr3 = BREAK_POINT.map((key) => {
         const simpleCompletion = new vscode.CompletionItem({
@@ -57,20 +105,42 @@ export function activate(context: vscode.ExtensionContext) {
         return simpleCompletion;
       });
 
-      return [...arr1, ...arr2, ...arr3];
+      return arr3;
     },
   };
 
-  const subscription = vscode.languages.registerCompletionItemProvider(
+  const subscription1 = vscode.languages.registerCompletionItemProvider(
     [
       { language: 'typescript', scheme: 'file' },
       { language: 'typescriptreact', scheme: 'file' },
       { language: 'javascript', scheme: 'file' },
       { language: 'javascriptreact', scheme: 'file' },
     ],
-    provider,
+    providerCssProps,
     ' ',
   );
+  const subscription2 = vscode.languages.registerCompletionItemProvider(
+    [
+      { language: 'typescript', scheme: 'file' },
+      { language: 'typescriptreact', scheme: 'file' },
+      { language: 'javascript', scheme: 'file' },
+      { language: 'javascriptreact', scheme: 'file' },
+    ],
+    providerCssPseudo,
+    '|',
+  );
+  const subscription3 = vscode.languages.registerCompletionItemProvider(
+    [
+      { language: 'typescript', scheme: 'file' },
+      { language: 'typescriptreact', scheme: 'file' },
+      { language: 'javascript', scheme: 'file' },
+      { language: 'javascriptreact', scheme: 'file' },
+    ],
+    providerCssBreakpoint,
+    '@',
+  );
 
-  context.subscriptions.push(subscription);
+  context.subscriptions.push(subscription1);
+  context.subscriptions.push(subscription2);
+  context.subscriptions.push(subscription3);
 }
